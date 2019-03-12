6a939e9760de3e6e217bfc5c1607980fcc4d6669
David Proksch <david.proksch@gmail.com>
2019-03-12 18:16:27 -0400

    Update CreateTable.sql

--- CreateTable.sql
+++ CreateTable.sql
@@ -6,3 +6,26 @@
 -- It is important to note what IS MISSING:  Indexes, Partitioning Key, 
 -- HASH/DISTRIBUTION KEYS!
 --
+create or replace table crimes (
+ id float NOT NULL
+, case_number STRING
+, crime_date STRING
+, block STRING
+, iucr STRING
+, primary_type STRING
+, crime_desc STRING
+, location_desc STRING
+, arrest STRING
+, domestic STRING
+, beat STRING
+, district STRING
+, ward STRING
+, community_area STRING
+, fbi_code STRING
+, x_coord FLOAT
+, y_coord FLOAT
+, year INTEGER
+, updated_on STRING
+, latitude FLOAT
+, longitude FLOAT
+, location STRING)
