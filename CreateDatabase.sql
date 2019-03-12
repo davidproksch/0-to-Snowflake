3b2b9b1600c655fc03a3b99296dce5fb143c0324
David Proksch <david.proksch@gmail.com>
2019-03-12 17:03:55 -0400

    Adding CreateDatabase.sql

---
+++ CreateDatabase.sql
@@ -0,0 +1,6 @@
+--
+-- Sample DDL to create a database.
+-- In this sample, the database name is "zero2snowflake".
+-- The comment field is filled out, but COMMENT = <Comment> is optional
+--
+CREATE DATABASE zero2snowflake COMMENT = 'Zero To Snowflake DB';
